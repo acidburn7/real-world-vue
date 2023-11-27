2f680fd84e2f37971b28b93280671cc6ec7dfb8f
acidburn7 <varaksinaalla@mail.ru>
2023-11-27 14:46:15 +0300

    dynamic routing

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import EventListView from '../views/EventListView.vue'
 import AboutView from '../views/AboutView.vue'
+import EventDetailsView from '../views/EventDetailsView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -18,6 +19,12 @@ const router = createRouter({
       // which is lazy-loaded when the route is visited.
       //component: () => import('../views/AboutView.vue')
       component: AboutView
+    },
+    {
+      path: '/event/:id',
+      name: 'event-details',
+      props: true,
+      component: EventDetailsView
     }
   ]
 })
